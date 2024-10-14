34746282b8e711502cbbe641ea21410fc3f59193
عبدالرحمن الرداد <74423141+abdoelradad@users.noreply.github.com>
2024-10-14 19:14:32 +0300

    Some more style like button and links with reuseable components

---
+++ src/ui/LinkButton.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+function LinkButton({ children, to }) {
+  const navigate = useNavigate();
+
+  const classname = "text-sm text-blue-500 hover:text-blue-600 hover:underline";
+  if (to === "-1") {
+    return (
+      <button onClick={() => navigate(-1)} className={classname}>
+        {children}
+      </button>
+    );
+  }
+  return (
+    <Link to={to} className={classname}>
+      {children}
+    </Link>
+  );
+}
+
+export default LinkButton;
